bb2d2a04bad25fc0fcd5af6218ed6716cc95519a
Thirunavukarasu-jpeg <thirunavukarasu@Masterusers-MacBook-Pro.local>
2023-08-02 22:39:36 +0530

    Modified the service frequency period for the Spindler battery.

--- battery/model/spindler_battery.py
+++ battery/model/spindler_battery.py
@@ -6,7 +6,7 @@ class SpindlerBattery(Battery):
     def __init__(self,last_service_date: date,current_date: date) -> None:
         self.last_service_date = last_service_date
         self.current_date = current_date
-        self.service_frequency = 2
+        self.service_frequency = 3
     def needs_service(self) -> bool:
         service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + self.service_frequency)
         if service_threshold_date < datetime.today().date():
